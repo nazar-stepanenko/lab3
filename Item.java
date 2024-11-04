db1a210be9258907c3afc3a3500845464c5ba322
nazar-stepanenko <146169911+nazar-stepanenko@users.noreply.github.com>
2024-11-05 01:02:38 +0200

    Використання List<Item> замість Item[] зробило код більш гнучким та зручним для маніпуляцій та розширення коду у подальшому. Таким чином задовольнив метод. First Class Collections. Загалом в цьому коді вже були реалізовані методи Only One Level Of Indentation Per Method, One Dot Per Line, Don’t Abbreviate та Keep All Entities Small. Хоча вказано уникати геттерів, це єдиний спосіб отримати значення, враховуючи принцип Wrap All Primitives And Strings.

--- Item.java
+++ Item.java
@@ -1,16 +1,35 @@
 package lab3;
 
-public class Item {
+import java.util.List;
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
+class Item {
+    private String name;
+    private int sellIn;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
         this.quality = quality;
     }
 
