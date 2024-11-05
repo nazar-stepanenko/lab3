fc4c910edced1ccf6c8be80717da9d498d794110
nazar-stepanenko <146169911+nazar-stepanenko@users.noreply.github.com>
2024-11-05 14:20:37 +0200

    Позбувся риси ''смердючого'' коду - Large Class. Тепер код простіше удосконалювати. Також у такий спосіб задовольнив метод One Dot Per Line.

--- GildedRose.java
+++ GildedRose.java
@@ -1,100 +1,125 @@
 package lab3;
 
 class GildedRose {
+
     Item[] items;
+    NameCheck nameCheck = new NameCheck();
+    QualityControl qualityControl = new QualityControl();
+    SellInControl sellInControl = new SellInControl();
+    UpdateQuality updateQuality = new UpdateQuality();
+    UpdateSellIn updateSellIn = new UpdateSellIn();
 
     public GildedRose(Item[] items) {
+
         this.items = items;
     }
 
     public void updateQuality() {
+
         for (int i = 0; i < items.length; i++) {
-            nameCheck1(i);
-            nameCheck2(i);
-            qualityControl_50(i);
-            nameCheck3(i);
-            sellInControl_11(i);
-            sellInControl_6(i);
-            nameCheck4(i);
-            qualityControl_0(i);
-            sellInControl_0(i);
-            updateQualityMinus(i);
-            updateQualityPlus(i);
-            updateQuality_1(i);
-            updateSallinMinus(i);
+
+            nameCheck.nameCheck1(items[i], qualityControl, updateQuality);
+            nameCheck.nameCheck2(items[i], updateQuality);
+            qualityControl.qualityControl_50(items[i], updateQuality);
+            nameCheck.nameCheck3(items[i], sellInControl, qualityControl);
+            sellInControl.sellInControl_11(items[i], qualityControl);
+            sellInControl.sellInControl_6(items[i], qualityControl);
+            nameCheck.nameCheck4(items[i], sellInControl);
+            qualityControl.qualityControl_0(items[i], nameCheck, updateQuality);
+            sellInControl.sellInControl_0(items[i], nameCheck);
+            updateQuality.updateQualityMinus(items[i]);
+            updateQuality.updateQualityPlus(items[i]);
+            updateQuality.updateQuality_1(items[i]);
+            updateSellIn.updateSellInMinus(items[i]);
         }
-    }
-
-    public void nameCheck1(int i) {
-        if (!items[i].name.equals("Aged Brie")
-                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            qualityControl_0(i);
-        }
-    }
-
-    public void nameCheck2(int i) {
-        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            updateQualityMinus(i);
-        }
-    }
-
-    public void nameCheck3(int i) {
-        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            sellInControl_11(i);
-            sellInControl_6(i);
-        }
-    }
-
-    public void nameCheck4(int i) {
-        if (!items[i].name.equals("Aged Brie")) {
-            sellInControl_0(i);
-        }
-    }
-
-    public void qualityControl_0(int i) {
-        if (items[i].quality > 0) {
-            nameCheck2(i);
-        }
-    }
-
-    public void qualityControl_50(int i) {
-        if (items[i].quality < 50) {
-            updateQualityPlus(i);
-        }
-    }
-
-    public void sellInControl_11(int i) {
-        if (items[i].sellIn < 11) {
-            qualityControl_50(i);
-        }
-    }
-
-    public void sellInControl_6(int i) {
-        if (items[i].sellIn < 6) {
-            qualityControl_50(i);
-        }
-    }
-
-    public void sellInControl_0(int i) {
-        if (items[i].sellIn < 0) {
-            nameCheck4(i);
-        }
-    }
-
-    public void updateQualityMinus(int i) {
-        items[i].quality = items[i].quality - 1;
-    }
-
-    public void updateQualityPlus(int i) {
-        items[i].quality = items[i].quality + 1;
-    }
-
-    public void updateQuality_1(int i) {
-        items[i].quality = 0;
-    }
-
-    public void updateSallinMinus(int i) {
-        items[i].sellIn = items[i].sellIn - 1;
     }
 }
 
+class NameCheck {
+
+    public void nameCheck1(Item item, QualityControl qualityControl, UpdateQuality updateQuality) {
+
+        if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+            qualityControl.qualityControl_0(item, this, updateQuality);
+    }
+
+    public void nameCheck2(Item item, UpdateQuality updateQuality) {
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+            updateQuality.updateQualityMinus(item);
+    }
+
+    public void nameCheck3(Item item, SellInControl sellInControl, QualityControl qualityControl) {
+
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            sellInControl.sellInControl_11(item, qualityControl);
+            sellInControl.sellInControl_6(item, qualityControl);
+        }
+    }
+
+    public void nameCheck4(Item item, SellInControl sellInControl) {
+
+        if (!item.name.equals("Aged Brie"))
+            sellInControl.sellInControl_0(item, this);
+    }
+}
+
+class QualityControl {
+
+    public void qualityControl_0(Item item, NameCheck nameCheck, UpdateQuality updateQuality) {
+        if (item.quality > 0)
+
+            nameCheck.nameCheck2(item, updateQuality);
+    }
+
+    public void qualityControl_50(Item item, UpdateQuality updateQuality) {
+        if (item.quality < 50)
+
+            updateQuality.updateQualityPlus(item);
+    }
+}
+
+class SellInControl {
+    public void sellInControl_11(Item item, QualityControl qualityControl) {
+
+        if (item.sellIn < 11)
+            qualityControl.qualityControl_50(item, new UpdateQuality());
+    }
+
+    public void sellInControl_6(Item item, QualityControl qualityControl) {
+
+        if (item.sellIn < 6)
+            qualityControl.qualityControl_50(item, new UpdateQuality());}
+    }
+
+    public void sellInControl_0(Item item, NameCheck nameCheck) {
+
+        if (item.sellIn < 0)
+            nameCheck.nameCheck4(item, this);
+    }
+}
+
+class UpdateQuality {
+
+    public void updateQualityMinus(Item item) {
+        item.quality = item.quality - 1;
+    }
+
+    public void updateQualityPlus(Item item) {
+
+        item.quality = item.quality + 1;
+    }
+
+    public void updateQuality_1(Item item) {
+
+        item.quality = 0;
+    }
+}
+
+class UpdateSellIn {
+
+    public void updateSellInMinus(Item item) {
+
+        item.sellIn = item.sellIn - 1;
+    }
+}
